d2db3928e321843f2c573fb49438a1b77c947f43
ElangovanPro <elangovanvcet@gmail.com>
2025-08-05 10:25:32 -0500

    secondGreatest

---
+++ src/programs/findSecondGreatest.js
@@ -0,0 +1,25 @@
+//write a function to return the second greatest number in an array
+function findGreatest(arr){
+    if(arr.length<2){
+     return "Please enter atleast 2 numbers";
+    }
+    let max=-Infinity;
+    let secondMax=-Infinity;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>max)
+    { 
+     secondMax=max;//-inf,4,
+     max=arr[i];// 4,9,
+    }
+    else if(arr[i]>secondMax)
+    {
+    secondMax=arr[i];
+    }
+}
+return secondMax;
+}
+// let arr=[1,2,5,6,7,9];
+// let arr=[-5,-4,-3,-2,-1];
+let arr=[4,9,0,2,8,7,1];
+// let arr=[4];
+console.log(findGreatest(arr));
