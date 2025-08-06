50d3bf68e45a0fb2b9d2968baaf00d3da6bed74b
ElangovanPro <elangovanvcet@gmail.com>
2025-08-06 10:28:12 -0500

    ridestarpattern

--- src/programs/rightStarPattern.js
+++ src/programs/rightStarPattern.js
@@ -7,11 +7,11 @@ for(let j=0;j<n-i-1;j++)
 {
 row=row+" ";
 }
-let row2="";
+let star="";
 for(let k=0;k<=i;k++){
-row2=row2+"*";
+star=star+"*";
 }
-console.log(row+row2);
+console.log(row+star);
 }
 }
 rightStarPattern(5);
